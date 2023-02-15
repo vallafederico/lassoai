88439d51cd104d498928ca2613664b9848627ca3
vallafederico <valla.federico@gmail.com>
2023-02-15 13:05:27 +0100

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      aspectRatio: {
+        imgv: "5/7",
+        imgh: "7/5",
+      },
+    },
+    colors: {
+      black: "#121218",
+      white: "#F5F5F5",
+      lg: "#99A0A9",
+      yellow: "#EDFF00",
+    },
+  },
+  plugins: [],
+};
